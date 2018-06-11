7b5bcba7d9dc7471ec0255ca16aef180c6444627
mahrous <your_email_address@example.com>
2018-06-11 15:47:49 +0300

    palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,11 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+const palindrome = str =>
+  str ===
+  str
+    .split("")
+    .reverse()
+    .join("");
 
 module.exports = palindrome;
