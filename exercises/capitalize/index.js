a8e494aa1da2ef2ba4b2a1d36b55eac6bab67407
mahrous <your_email_address@example.com>
2018-09-10 15:29:47 +0300

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,21 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// function capitalize(str) {
+//   const words = [];
+//   const arry = str.split(' ');
+//   for (let word of arry) {
+//     words.push(word[0].toUpperCase() + word.slice(1));
+//   }
+//   return words.join(' ');
+// }
+
+const capitalize = str => {
+  let result = str[0].toUpperCase();
+  for (let i = 1, j = str.length; i < j; i++) {
+    result += str[i - 1] === ' ' ? str[i].toUpperCase() : str[i];
+  }
+  return result;
+};
 
 module.exports = capitalize;
